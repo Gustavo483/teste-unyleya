5b5f75416aa8e76f58c0dc2bac4a3ee0f27a2fba
Gustavo483 <gu.moura01@gmail.com>
2022-10-13 08:12:28 -0300

    Inicialização do projeto laravel e instalação do vue.js

---
+++ resources/js/router.js
@@ -0,0 +1,19 @@
+import {createWebHistory, createRouter} from "vue-router";
+
+import home from './pages/home.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: home
+    },
+
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
